a940f27ed935375ddac0715323ba6261494d7b0f
martinjensen37 <martinjensen37@gmail.com>
2024-12-16 14:52:15 +0100

    changed hostname

--- scripts/random_opc-ua_server.py
+++ scripts/random_opc-ua_server.py
@@ -6,7 +6,7 @@ import time
 server = Server()
 
 # Set the endpoint for the server
-server.set_endpoint("opc.tcp://localhost:4840/freeopcua/server/") # opc.tcp://192.168.0.108:4840/freeopcua/server/RandomValue1
+server.set_endpoint("opc.tcp://0.0.0.0:4840/freeopcua/server/") # opc.tcp://192.168.0.108:4840/freeopcua/server/RandomValue1
 
 # Register a new namespace
 uri = "http://examples.freeopcua.github.io"
@@ -28,7 +28,7 @@ var2.set_writable()
 
 # Start the server
 server.start()
-print("Server started at opc.tcp://localhost:4840/freeopcua/server/")
+print("Server started at opc.tcp://0.0.0.0:4840/freeopcua/server/")
 
 try:
     while True:
