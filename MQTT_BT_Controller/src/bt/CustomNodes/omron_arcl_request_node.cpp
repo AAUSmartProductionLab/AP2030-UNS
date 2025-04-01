acea3793e6e3a7f2609f1863c14b472fac19a245
Tristan Schwörer <tristans@mp.aau.dk>
2025-04-01 11:16:17 +0200

    Applied refactor to Omron Node

--- MQTT_BT_Controller/src/bt/CustomNodes/omron_arcl_request_node.cpp
+++ MQTT_BT_Controller/src/bt/CustomNodes/omron_arcl_request_node.cpp
@@ -12,7 +12,7 @@ OmronArclRequest::OmronArclRequest(const std::string &name, const BT::NodeConfig
 {
     if (MqttActionNode::subscription_manager_)
     {
-        MqttActionNode::subscription_manager_->registerDerivedInstance<OmronArclRequest>(this);
+        MqttActionNode::subscription_manager_->registerDerivedInstance(this);
     }
 }
 
@@ -45,41 +45,9 @@ bool OmronArclRequest::isInterestedIn(const std::string &field, const json &valu
 void OmronArclRequest::callback(const json &msg, mqtt::properties props)
 {
     {
-        std::lock_guard<std::mutex> lock(state_mutex_);
-        state = BT::NodeStatus::SUCCESS;
+        // Todo implment state management
+        std::lock_guard<std::mutex> lock(mutex_);
+        setStatus(BT::NodeStatus::SUCCESS);
         emitWakeUpSignal();
-        state_updated_.store(true, std::memory_order_seq_cst);
-        // Update state based on message content
-        // if (msg.contains("message"))
-        // {
-        //     if (msg["message"] == 0)
-        //     {
-        //         current_command_uuid_ = "";
-        //         state = BT::NodeStatus::FAILURE;
-        //         emitWakeUpSignal();
-        //     }
-        //     else if (msg["message"] == 0)
-        //     {
-        //         current_command_uuid_ = "";
-        //         state = BT::NodeStatus::SUCCESS;
-        //         emitWakeUpSignal();
-        //     }
-        //     else if (msg["message"] == 0)
-        //     {
-        //         state = BT::NodeStatus::RUNNING;
-        //         emitWakeUpSignal();
-        //     }
-        //     else
-        //     {
-        //         std::cout << "Unknown State value: " << msg["State"] << std::endl;
-        //     }
-
-        //     // Use explicit memory ordering when setting the flag
-        //     state_updated_.store(true, std::memory_order_seq_cst);
-        // }
-        // else
-        // {
-        //     std::cout << "Message doesn't contain 'state' field" << std::endl;
-        // }
     }
 }
